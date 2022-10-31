471d0aa824fbdf1b1e36a3d4363bc660cfbf4caa
Kat Seitz <katharina.seitz@northwestern.edu>
2022-10-31 10:45:55 -0500

    studies dir script now handles multiple session folders

--- scripts/to_studies_dir.py
+++ scripts/to_studies_dir.py
@@ -14,18 +14,19 @@ def mover():
     for partic in os.listdir(work_dir):
         try:
             if(partic[0:5] == "sub-t"):
-                wd = work_dir + partic + "/ses-1/"
-                for folder in os.listdir(wd): 
-                #checks to make sure it's a participant
-                    source = wd + folder
-                    if(folder == "beh"):
-                        dest = "/projects/b1108/studies/transitions/data/raw/neuroimaging/behavioral/"\
-                            + partic + "/ses-1/" + folder
-                        shutil.copytree(source, dest) 
-                    else:
-                        dest = "/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/"\
-                             + partic + "/ses-1/" + folder
-                        shutil.copytree(source, dest) 
+                for ses in os.listdir(work_dir + partic):
+                wd = work_dir + partic + "/" + ses
+                    for folder in os.listdir(wd): 
+                    #checks to make sure it's a participant
+                        source = wd + folder
+                        if(folder == "beh"):
+                            dest = "/projects/b1108/studies/transitions/data/raw/neuroimaging/behavioral/"\
+                                + partic + "/" + ses + "/" + folder
+                            shutil.copytree(source, dest) 
+                        else:
+                            dest = "/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/"\
+                                + partic + "/" + ses + "/" + folder
+                            shutil.copytree(source, dest) 
                 print("Copied succesfully: " + partic)
         except:
             print("FAILED: " + partic)
