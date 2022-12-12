dff37aa2831cc7b18cbdf896d9834088904e61f4
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2022-12-12 14:39:13 -0600

    bidskit for trans.

--- scripts/bidskit.sh
+++ scripts/bidskit.sh
@@ -1,12 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
+#SBATCH --account=p31833 ## Required: your allocation/account name, i.e. eXXXX, pXXXX or bXXXX
+#SBATCH --partition=normal ## Required: (buyin, short, normal, long, gengpu, genhimem, etc)
+#SBATCH --time=2:00:00 ## Required: How long will the job need to run (remember different partitions have restrictions on this parameter)
+#SBATCH --nodes=1 ## how many computers/nodes do you need (no default)
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on per computer/node (default value 1)
+#SBATCH --mem=32G ## how much RAM do you need per computer/node (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=transitions_bidskit ## When you run squeue -u  this is how you can identify the job
+#SBATCH --output=output.log ## standard out and standard error goes to this file
 
-#SBATCH -A p31833
-#SBATCH -p normal
-#SBATCH -t 4:00:00
-#SBATCH --mem=64G
-#SBATCH -J bidskit_sourcedata
+# A regular comment in Bash
 
 module load dcm2niix
-bidskit -d /projects/b1108/studies/transitions/data/raw/neuroimaging/bids
-
-
+bidskit -d /projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata
