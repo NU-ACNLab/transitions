fbd8b536cd7349763f645b3f6f76fa36781762af
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2023-01-31 08:48:43 -0600

    adding intended for script

---
+++ scripts/fmriprep/add_intended_for.py
@@ -0,0 +1,26 @@
+import os
+import glob
+import json
+
+subjectsPath = "/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sub-*"
+subjects = glob.glob(subjectsPath)
+
+
+for subject in subjects:
+
+	#Edit 'ses-1' to be the same as your session
+	fmapsPath = os.path.join(subject, 'ses-1', 'fmap', '*.json')
+	fmaps = glob.glob(fmapsPath)
+	funcsPath = os.path.join(subject, 'ses-1', 'func', '*.nii.gz')
+	funcs = glob.glob(funcsPath)
+
+	#substring to be removed from absolute path of functional files
+	pathToRemove = subject + '/'
+	funcs = list(map(lambda x: x.replace(pathToRemove, ''), funcs))
+	for fmap in fmaps:
+		with open(fmap, 'r') as data_file:
+			fmap_json = json.load(data_file)
+		fmap_json['IntendedFor'] = funcs
+
+		with open(fmap, 'w') as data_file:
+			fmap_json = json.dump(fmap_json, data_file)
