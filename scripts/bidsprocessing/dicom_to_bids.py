24bf49f2f0d0e22bd3a353791272ad07a63739f5
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2023-02-16 16:40:41 -0600

    adapting tabulate script for HARP

--- scripts/bidsprocessing/dicom_to_bids.py
+++ scripts/bidsprocessing/dicom_to_bids.py
@@ -1,38 +1,171 @@
+### This script is designed to replace heudiconv because it is bugging out in
+### far too many ways.
+###
+### Ellyn Butler
+### Feb 16, 2023
+
+import pydicom #https://github.com/pydicom/pydicom
+# https://pydicom.github.io/pydicom/stable/old/getting_started.html
 import os
-import glob
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from nipype.interfaces.dcm2nii import Dcm2niix
 import json
-import shutil
+import argparse
 
-##Deal with that we're in an infinite loop
-compressed_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/"
-compressed_files = glob.glob(compressed_path + "*" )
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', default='/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/uncompressed')
+parser.add_argument('-o', default='/projects/b1108/studies/transitions2/data/raw/neuroimaging/bids')
+parser.add_argument('-s')
+parser.add_argument('-ss')
+args = parser.parse_args()
 
+indir = args.i
+outdir = args.o
+sub = args.s
+ses = args.ss
 
-for compressed in compressed_files:
-    if(not(compressed == "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/complete")):
-        print(compressed)
-        #grabs subject id from compressed file name
-        subject = compressed.split("/")[-1][0:5].lower()
-        print(subject)
-        #unzip/untar into participant dir
-        uncom_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/uncompressed/"
-        shutil.unpack_archive(compressed, uncom_path + subject)
-        if(not(os.path.exists(uncom_path + subject + "/" + subject))):
-            print("moving " + subject)
-            next_level = glob.glob(uncom_path + subject + "/*")[0]
-            dest = uncom_path + subject + "/1"
-            os.makedirs(dest, exist_ok=True)
-            for file in glob.glob(next_level + "/s*/*"):
-                shutil.move(file, dest)
-            for folder in glob.glob(uncom_path + subject + "/e*/s*"):
-                os.rmdir(folder)
-            #os.rmdir(glob.glob(uncom_path + subject + "/e*")[0])
-        else:
-            print("You gotta do this one manually: " + subject)
-        #com_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/"
-        shutil.move(compressed, "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/complete")
-
-    
+def convert_dicoms(dicomdir, bidsdir, modality):
+    converter = Dcm2niix()
+    converter.inputs.source_dir = dicomdir
+    converter.inputs.compression = 5
+    converter.inputs.output_dir = bidsdir+'/'+modality
+    converter.run()
 
 
+def curate_scan(sub, ses, scan, indir):
+    dicomdir = indir+'/'+sub+'/'+ses+'/SCANS/'+scan+'/DICOM'
+    dcm_path = os.popen('find '+dicomdir+' -name "*.dcm"').read().split("\n")[0]
+    dicoms = os.popen('find '+dicomdir+' -name "*.dcm"').read().split("\n")[:-1]
+    if len(dcm_path) == 0:
+        dcm_path = os.popen('find '+dicomdir+' -regex ".*/[0-9]+"').read().split("\n")[0]
+        dicoms = os.popen('find '+dicomdir+' -regex ".*/[0-9]+"').read().split("\n")[:-1]
+    ndicoms = len(dicoms)
+    # If the sub outdir doesn't exist, make it
+    if not os.path.isdir(outdir+'/'+sub):
+        os.mkdir(outdir+'/'+sub)
+    # If the ses outdir doesn't exist, make it
+    if not os.path.isdir(outdir+'/'+sub+'/'+ses):
+        os.mkdir(outdir+'/'+sub+'/'+ses)
+    bidsdir = outdir+'/'+sub+'/'+ses
+    if ndicoms > 10:
+        dcm = pydicom.dcmread(dcm_path)
+        # T1w image
+        if (('l_epinav_ME2' in dcm.ProtocolName) or 'MPRAGE_SAG_0.8iso' in dcm.ProtocolName) and (ndicoms == 208) and (dcm.AcquisitionMatrix[1] == 320):
+            # If the modality directory doesn't exist, make it
+            modality = 'anat'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            # Check if the files already exist
+            if os.path.exists(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.json'):
+                oldjson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                oldjson = json.load(open(oldjson))
+                if int(oldjson['SeriesNumber']) < int(scan):
+                    # If they do, and the series number in the json header for the already
+                    # converted image is smaller, delete the old output, and convert
+                    # the new one.
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.json')
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.nii.gz')
+                    convert_dicoms(dicomdir, bidsdir, modality)
+                    filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                    os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.json')
+                    nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*.nii.gz"').read().split("\n")[0]
+                    os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.nii.gz')
+                    # ^^^ THIS IS ASSUMING THAT A T1W IMAGE THAT WAS REDONE
+                    # WOULD BE BETTER THE SECOND TIME.
+            else:
+                convert_dicoms(dicomdir, bidsdir, modality)
+                filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.json')
+                nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*.nii.gz"').read().split("\n")[0]
+                os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_T1w.nii.gz')
+        # DTI 9 (10-13 derived)
+        elif ('DTI_MB4_68dir_1pt5mm_b1k' == dcm.SeriesDescription) and (ndicoms > 60) and (ndicoms < 70) and (dcm.SliceThickness == 1.5) and (dcm.RepetitionTime == 2500):
+            modality = 'dwi'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            # Rename files to be BIDS compliant
+            # Check if the files already exist
+            if os.path.exists(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_dwi.json'):
+                oldjson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                oldjson = json.load(open(oldjson))
+                if int(oldjson['SeriesNumber']) < int(scan):
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_dwi.json')
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_dwi.nii.gz')
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_dwi.bvec')
+                    os.remove(bidsdir+'/'+modality+'/'+sub+'_'+ses+'_dwi.bval')
+                    convert_dicoms(dicomdir, bidsdir, modality)
+                    filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                    os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.json')
+                    nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*.nii.gz"').read().split("\n")[0]
+                    os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.nii.gz')
+                    bvec = os.popen('find '+bidsdir+'/'+modality+' -name "*.bvec"').read().split("\n")[0]
+                    os.rename(bvec, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bvec')
+                    bval = os.popen('find '+bidsdir+'/'+modality+' -name "*.bval"').read().split("\n")[0]
+                    os.rename(bval, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bval')
+            else:
+                convert_dicoms(dicomdir, bidsdir, modality)
+                filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*.json"').read().split("\n")[0]
+                os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.json')
+                nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*.nii.gz"').read().split("\n")[0]
+                os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.nii.gz')
+                bvec = os.popen('find '+bidsdir+'/'+modality+' -name "*.bvec"').read().split("\n")[0]
+                os.rename(bvec, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bvec')
+                bval = os.popen('find '+bidsdir+'/'+modality+' -name "*.bval"').read().split("\n")[0]
+                os.rename(bval, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_'+modality+'.bval')
+        # faces 16 (15 for 181 1)
+        elif (('FACES' in dcm.ProtocolName) or ('MB2_task' in dcm.ProtocolName)) and (ndicoms < 205) and (ndicoms > 195) and (dcm.SliceThickness < 1.8):
+            modality = 'func'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            convert_dicoms(dicomdir, bidsdir, modality)
+            filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*FACES*.json"').read().split("\n")[0]
+            if len(filejson) == 0:
+                filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*MB2_task*.json"').read().split("\n")[0]
+            json_obj = json.load(open(filejson, 'r'))
+            json_obj['TaskName'] = 'faces'
+            with open(filejson, 'w') as f:
+                json.dump(json_obj, f)
+            os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-faces_bold.json')
+            nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*FACES*.nii.gz"').read().split("\n")[0]
+            if len(nifti) == 0:
+                nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*MB2_task*.nii.gz"').read().split("\n")[0]
+            os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-faces_bold.nii.gz')
+        # passive avoidance 17
+        elif (('PASSIVE' in dcm.ProtocolName) or ('MB2_task' in dcm.ProtocolName)) and (ndicoms > 295) and (ndicoms < 305) and (dcm.SliceThickness < 1.8):
+            modality = 'func'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            convert_dicoms(dicomdir, bidsdir, modality)
+            filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*PASSIVE*.json"').read().split("\n")[0]
+            if len(filejson) == 0:
+                filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*MB2_task*.json"').read().split("\n")[0]
+            json_obj = json.load(open(filejson, 'r'))
+            json_obj['TaskName'] = 'avoid'
+            with open(filejson, 'w') as f:
+                json.dump(json_obj, f)
+            os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-avoid_bold.json')
+            nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*PASSIVE*.nii.gz"').read().split("\n")[0]
+            if len(nifti) == 0:
+                nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*MB2_task*.nii.gz"').read().split("\n")[0]
+            os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-avoid_bold.nii.gz')
+        # resting state 18
+        elif ('Mb8_rest_HCP' in dcm.ProtocolName) and (dcm.SliceThickness == 2):
+            modality = 'func'
+            if not os.path.isdir(bidsdir+'/'+modality):
+                os.mkdir(bidsdir+'/'+modality)
+            convert_dicoms(dicomdir, bidsdir, modality)
+            filejson = os.popen('find '+bidsdir+'/'+modality+' -name "*Mb8_rest_HCP*.json"').read().split("\n")[0]
+            json_obj = json.load(open(filejson, 'r'))
+            json_obj['TaskName'] = 'rest'
+            with open(filejson, 'w') as f:
+                json.dump(json_obj, f)
+            os.rename(filejson, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-rest_bold.json')
+            nifti = os.popen('find '+bidsdir+'/'+modality+' -name "*Mb8_rest_HCP*.nii.gz"').read().split("\n")[0]
+            os.rename(nifti, bidsdir+'/'+modality+'/'+sub+'_'+ses+'_task-rest_bold.nii.gz')
 
+scans = os.listdir(indir+'/'+sub+'/'+ses+'/SCANS/')
+scans = [item for item in scans if 'DICOM' in os.listdir(indir+'/'+sub+'/'+ses+'/SCANS/'+item)]
+for scan in scans:
+    curate_scan(sub, ses, scan, indir)
---
+++ scripts/bidsprocessing/unzip_dicoms.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import json
+import shutil
+
+##Deal with that we're in an infinite loop
+compressed_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/"
+compressed_files = glob.glob(compressed_path + "*" )
+
+
+for compressed in compressed_files:
+    if(not(compressed == "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/complete")):
+        print(compressed)
+        #grabs subject id from compressed file name
+        subject = compressed.split("/")[-1][0:5].lower()
+        print(subject)
+        #unzip/untar into participant dir
+        uncom_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/uncompressed/"
+        shutil.unpack_archive(compressed, uncom_path + subject)
+        if(not(os.path.exists(uncom_path + subject + "/" + subject))):
+            print("moving " + subject)
+            next_level = glob.glob(uncom_path + subject + "/*")[0]
+            dest = uncom_path + subject + "/1"
+            os.makedirs(dest, exist_ok=True)
+            for file in glob.glob(next_level + "/s*/*"):
+                shutil.move(file, dest)
+            for folder in glob.glob(uncom_path + subject + "/e*/s*"):
+                os.rmdir(folder)
+            #os.rmdir(glob.glob(uncom_path + subject + "/e*")[0])
+        else:
+            print("You gotta do this one manually: " + subject)
+        #com_path = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/"
+        shutil.move(compressed, "/projects/b1108/studies/transitions2/data/raw/neuroimaging/dicoms/compressed/complete")
+
+    
+
