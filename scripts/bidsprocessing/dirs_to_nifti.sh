35470d29564937dc4f4f571413d85185acd9352d
Kat Seitz <katharina.seitz@northwestern.edu>
2023-02-23 15:12:55 -0600

    pushing

--- scripts/bidsprocessing/dirs_to_nifti.sh
+++ scripts/bidsprocessing/dirs_to_nifti.sh
@@ -9,17 +9,19 @@
 module purge
 module load dcm2niix
 
-DIR=1 #check if $ needed. 
+DIR=$1 #check if $ needed. 
 echo $DIR
-SUB=2
+SUB=$2
 echo $SUB
 
-scan_folders = /projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata/$DIR* 
+scan_folders=/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata/$DIR/* 
+echo $scan_folders
 
-
-for SCAN in scan_folders; do # Whitespace-safe but not recursive.
-    OUTPUT=/projects/b1108/studies/transitions2/data/raw/neuroimaging/bids/$SUB/ses-1
-    dcm2niix -b, y -z o -w 1 -f %n--%d--s%s--e%e -o #$OUTPUT $SCAN
+for SCAN in $scan_folders; do # Whitespace-safe but not recursive.
+    echo $SCAN
+    OUTPUT=/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sub-$SUB/ses-1
+    echo $OUTPUT
+    dcm2niix -b y -z o -w 1 -f %n--%d--s%s--e%e -o $OUTPUT $SCAN
 done
 
 #Error: Unknown command line argument: '-b,'
