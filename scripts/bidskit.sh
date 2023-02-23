c9d66e445bff89dbb44f24aae758f24ce4aedd99
Kat Seitz <katharina.seitz@northwestern.edu>
2023-02-23 09:08:34 -0600

    adjusting memory util

--- scripts/bidskit.sh
+++ scripts/bidskit.sh
@@ -4,9 +4,8 @@
 #SBATCH --time=2:00:00 ## Required: How long will the job need to run (remember different partitions have restrictions on this parameter)
 #SBATCH --nodes=1 ## how many computers/nodes do you need (no default)
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on per computer/node (default value 1)
-#SBATCH --mem=32G ## how much RAM do you need per computer/node (this affects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --mem=2G ## how much RAM do you need per computer/node (this affects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=transitions_bidskit ## When you run squeue -u  this is how you can identify the job
-#SBATCH --output=output.log ## standard out and standard error goes to this file
 
 # A regular comment in Bash
 
