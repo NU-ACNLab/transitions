3ad940bb3e3b8f09ac1dc6a53eecd92129d39f12
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2023-02-23 09:41:25 -0600

    trying again in bash

--- scripts/bidsprocessing/dirs_to_nifti.sh
+++ scripts/bidsprocessing/dirs_to_nifti.sh
@@ -1,13 +1,21 @@
-import subprocess
-import sys
+#!/usr/bin/bash
 
-directory = sys.argv[1]
-participant = sys.argv[2]
+#SBATCH -A p31833
+#SBATCH -p normal
+#SBATCH -t 24:00:00
+#SBATCH --mem=10G
+#SBATCH -J python_caller
 
-scan_folders = glob.glob("/projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata/" + directory + "/*" )
+module purge
+import dcm2niix
 
-for scan in scan_folders:
-    output_dir = "/projects/b1108/studies/transitions2/data/raw/neuroimaging/bids/" + participant + "/ses-1"
-    input_dir = scan
-    base_cmd = "dcm2niix -b, y -z o -w 1 -f %n--%d--s%s--e%e -o "
-    subprocess.run([base_cmd, output_dir, input_dir])
+DIR = sys.argv[1]
+SUB = sys.argv[2]
+
+scan_folders = /projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata/$DIR* 
+
+
+for SCAN in scan_folders; do # Whitespace-safe but not recursive.
+    OUTPUT = /projects/b1108/studies/transitions2/data/raw/neuroimaging/bids/$SUB/ses-1
+    dcm2niix -b, y -z o -w 1 -f %n--%d--s%s--e%e -o $OUTPUT $SCAN
+done
