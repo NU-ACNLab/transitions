e7858b745e2a3ffc1878cebc113fc1c6ab6f04bb
katseitz <katharinaseitz@WITIPRLW06KP212W2.local>
2023-02-23 10:07:25 -0600

    trying again in bash

--- scripts/bidsprocessing/dirs_to_nifti.sh
+++ scripts/bidsprocessing/dirs_to_nifti.sh
@@ -7,15 +7,20 @@
 #SBATCH -J t_nifti
 
 module purge
-import dcm2niix
+module load dcm2niix
 
-DIR = sys.argv[1]
-SUB = sys.argv[2]
+DIR=$1
+SUB=$2
 
 scan_folders = /projects/b1108/studies/transitions/data/raw/neuroimaging/bids/sourcedata/$DIR* 
 
 
 for SCAN in scan_folders; do # Whitespace-safe but not recursive.
-    OUTPUT = /projects/b1108/studies/transitions2/data/raw/neuroimaging/bids/$SUB/ses-1
-    dcm2niix -b, y -z o -w 1 -f %n--%d--s%s--e%e -o $OUTPUT $SCAN
+    OUTPUT=/projects/b1108/studies/transitions2/data/raw/neuroimaging/bids/$SUB/ses-1
+    dcm2niix -b, y -z o -w 1 -f %n--%d--s%s--e%e -o #$OUTPUT $SCAN
 done
+
+#/var/spool/slurmd/job4423074/slurm_script: line 12: DIR: command not found
+#/var/spool/slurmd/job4423074/slurm_script: line 13: SUB: command not found
+#/var/spool/slurmd/job4423074/slurm_script: line 15: scan_folders: command not found
+#/var/spool/slurmd/job4423074/slurm_script: line 19: OUTPUT: command not found
